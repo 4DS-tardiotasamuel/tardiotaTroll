3cbfa0b48c88b87d0d7d71802ffb47d71d5e2439
Samuel <samuel.tardiota@buonarroti.tn.it>
2023-10-04 21:14:42 +0200

    feat: created attributes + constructor

--- src/Troll.java
+++ src/Troll.java
@@ -1,2 +1,11 @@
 public class Troll {
+    private int health;
+    private int atkDamage;
+
+    public Troll(int hp, int atk){
+        this.health = hp;
+        this. atkDamage = atk;
+    }
+
+
 }
