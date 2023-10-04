98e0eff67fe245f764738b68da342f5e6d09baa2
Samuel <samuel.tardiota@buonarroti.tn.it>
2023-10-04 21:24:12 +0200

    feat: new class player with constructor

---
+++ src/Player.java
@@ -0,0 +1,17 @@
+public class Player {
+
+    private int health;
+    private int atkDamage;
+    private int defence;
+    private String race;
+
+    public Player(int hp, int atk, int def, String race) throws Exception{
+        if(hp<0 || atk<0){
+            throw new Exception("attributi non validi");
+        }
+        this.health = hp;
+        this. atkDamage = atk;
+        this.defence = def;
+        this.race = race;
+    }
+}
